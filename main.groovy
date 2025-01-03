16262ccdf509a23e418c8949e43c868e55d1d1a1
l-konov <bojan39@mail.ru>
2025-01-03 22:38:05 +0300

    Initial commit

---
+++ main.groovy
@@ -0,0 +1,117 @@
+def args = "20"
+def inputValue = args.toInteger()
+if ((inputValue > 9999) || (inputValue < 1)) 
+{ 
+  println 'Введён аргумент менее 1 или более 9999. Введите аргумент в диапазоне от 1 до 9999.'
+  return
+} 
+charArr = args.toCharArray()
+def thousands, hundreds, tens, units = null
+switch (charArr.length) {
+  case 4:
+    thousands = charArr[0]
+    hundreds = charArr[1]
+    tens = charArr[2]
+    units = charArr[3]
+    break
+  case 3: 
+    hundreds = charArr[0]
+    tens = charArr[1]
+    units = charArr[2]
+    break
+  case 2: 
+    tens = charArr[0]
+    units = charArr[1]
+    break
+  case 1: 
+    units = charArr[0]
+    break
+  default:
+    println 'Ошибка разбора числа'
+    return
+}
+
+println "•${processThousands(thousands)}\u0483${processHundreds(hundreds)}${processTens(tens)}${processUnits(units)}•" //\u0483
+
+def processThousands(th){
+  if (th == null) {return ''}
+  else {return "҂${processUnits(th)}"}
+}
+
+def processUnits(u) {
+  int num = u?.toString().toInteger()
+  switch (num) {
+    case 1:
+      return 'а'
+    case 2:
+      return 'в'
+    case 3:
+      return 'г'
+    case 4:
+      return 'д'
+    case 5:
+      return 'е'
+    case 6:
+      return 's'
+    case 7:
+      return 'з'
+    case 8:
+      return 'и'
+    case 9:
+      return 'ѳ' //??
+    default:
+      return ''
+  }
+}
+
+def processHundreds(h) {
+  int num = h?.toString().toInteger()
+  switch (num) {
+    case 1:
+      return 'р'
+    case 2:
+      return 'с'
+    case 3:
+      return 'т'
+    case 4:
+      return 'у'
+    case 5:
+      return 'ф'
+    case 6:
+      return 'х'
+    case 7:
+      return 'ѱ'
+    case 8:
+      return 'ѿ'
+    case 9:
+      return 'ц' //??
+    default:
+      return ''
+  }
+}
+
+def processTens(t) {
+  int num = t?.toString().toInteger()
+  switch (num) {
+    case 1:
+      return 'і'
+    case 2:
+      return 'к'
+    case 3:
+      return 'л'
+    case 4:
+      return 'м'
+    case 5:
+      return 'н'
+    case 6:
+      return 'ѯ'
+    case 7:
+      return 'о'
+    case 8:
+      return 'п'
+    case 9:
+      return 'ч' 
+    default:
+      return ''
+  }
+}
